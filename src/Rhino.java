18d3785bf78ecf856bcb0e62fa9d0dc9cb483605
ErnestLiar <el657@cornell.edu>
2019-09-09 21:43:02 -0400

    initial commit

---
+++ src/Rhino.java
@@ -0,0 +1,7 @@
+
+public class Rhino {
+    /** NetId: el657, ny46. Time spent: hh hours, mm minutes. What I thought about this
+     * assignment: */
+    /** An instance maintains info about the rhino. */
+
+}
